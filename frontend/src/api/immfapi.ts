f28f18baf84d2f4e967cae7f7ffedd0d169096ed
maardal <15260204+maardal@users.noreply.github.com>
2026-02-17 23:34:48 +0100

    List out loved ones, create button shell to add hangout

---
+++ frontend/src/api/immfapi.ts
@@ -0,0 +1,5 @@
+import { FRIENDS_URL } from '$lib/config';
+
+export const createHangout = async (id: string) => {
+	console.log(`adding hangout for lovedone: ${id}`);
+};
---
+++ frontend/src/lib/components/LovedOneCard.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { LovedOne } from '$lib/types/types';
+	import { createHangout } from '../../api/immfapi';
+	const { loved } = $props<{ loved: LovedOne }>();
+</script>
+
+<section>
+	<p>{loved.relationship}: {loved.name} + {loved.lastHangout}</p>
+	<button
+		onclick={() => {
+			createHangout(loved.id);
+		}}
+		>Add hangout
+	</button>
+</section>
